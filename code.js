0c60d4c5f90e78765586777541dbaab55b9ac5a2
Doga Cambaz <D.Cambaz@student.tudelft.nl>
2021-05-08 18:17:04 +0200

    animation is added

--- code.js
+++ code.js
@@ -3,22 +3,50 @@ document.addEventListener('DOMContentLoaded', () => {
     const startBtn = document.querySelector('#start-button')
     const stopBtn = document.querySelector('#stop-button')
     const resetBtn = document.querySelector('#reset-button')
-    let secLeft = 20
-    secondsLeftDisplay.innerHTML = secLeft
-    
-    var Interval ;
+    let inputTime = 20
+    let secLeft = inputTime
+    if(secLeft<10) {
+        secondsLeftDisplay.innerHTML = '0'+ secLeft
+    }
+    else { secondsLeftDisplay.innerHTML = secLeft }
+    var Interval;
+
+    // Divide time left by the defined time limit.
+    function calculateTimeFraction() {
+        const rawTimeFraction = (secLeft) / inputTime;
+        return rawTimeFraction - (1 / inputTime) * (1 - rawTimeFraction);
+        }
+          
+      // Update the dasharray value as time passes, starting with 283
+      function setCircleDasharray() {
+        const circleDasharray = `${(
+          calculateTimeFraction() * 283
+        ).toFixed(0)} 283`;
+        document
+          .getElementById("timer-path-remaining")
+          .setAttribute("stroke-dasharray", circleDasharray);
+      }
+
     function countDown(){
-        Interval = setInterval(function(){
+        Interval = setInterval(() => {
+            secLeft -=1
             if(secLeft<=0) {
-                clearInterval(secLeft=0)
+                secondsLeftDisplay.innerHTML = '00'
+                secLeft=0
+                setCircleDasharray();
+                /*document.getElementById("sec-left").style.animation = "buzz 0.5";*/
+                document.getElementsByClassName("timer-path-remaining").setAttribute("color", "red");
+                clearInterval();
             }
-            if(secLeft<10) {
-                secondsLeftDisplay.innerHTML = '0'+ secLeft
+            else if(secLeft<10) {
+                secondsLeftDisplay.innerHTML = '0'+ secLeft;
+                setCircleDasharray();
             }
             else {
                 secondsLeftDisplay.innerHTML = secLeft
+                setCircleDasharray();
             }
-            secLeft -=1
+            
         },1000)
     }
     startBtn.addEventListener('click',countDown)
@@ -27,7 +55,13 @@ document.addEventListener('DOMContentLoaded', () => {
         })
     resetBtn.addEventListener('click',function() {
          clearInterval(Interval);
-         secLeft = 20
-         secondsLeftDisplay.innerHTML = secLeft
+         secLeft = inputTime;
+         if(secLeft<10) {
+            secondsLeftDisplay.innerHTML = '0'+ secLeft
+        }
+        else { secondsLeftDisplay.innerHTML = secLeft }
+        setCircleDasharray()
         })
+
+    
 })
